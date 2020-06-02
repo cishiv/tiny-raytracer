9b1084e38cf18448744fd40ec3dc8f877dd6c61f
cishiv <shivantheman@gmail.com>
2020-06-02 23:04:02 +0200

    got some spheres drawn. yay!

--- main.go
+++ main.go
@@ -10,19 +10,33 @@ import (
 )
 
 // Inspired by https://github.com/ssloy/tinyraytracer
+// https://github.com/huqa/tinyraytracer-go/blob/54817bbe85026965ad4d0725a1375705f82d8e09/internal/app/tinyraytracer-go/render.go#L110
+// https://github.com/ssloy/tinyraytracer/blob/bd36c9857305b3cbd06f5b768bb48a92df9ae68b/geometry.h
+// https://github.com/huqa/tinyraytracer-go/blob/master/internal/pkg/vector/vector.go
 
-func render() {
-	width := 1024
-	height := 768
+// Camera view angle
+const fieldOfView = math.Pi / 3.0
+const width = 1024
+const height = 768
 
+// Sample materials
+var ivory = NewMaterial(0.4, 0.4, 0.3)
+var redRubber = NewMaterial(0.3, 0.1, 0.1)
+
+func render(spheres []Sphere) {
 	// Construct the framebuffer
 	framebuffer := make([]Vec3, width*height)
-
+	origin := NewVec3(0, 0, 0)
 	// Fill the framebuffer
 	fmt.Println("Filling framebuffer")
-	for i := 0; i < height; i++ {
-		for j := 0; j < width; j++ {
-			framebuffer[j+i*width] = NewVec3(float64(i)/float64(height), float64(j)/float64(width), 0)
+	for j := 0; j < height; j++ {
+		for i := 0; i < width; i++ {
+			// Big math (TODO: Figure this out)
+			x := (float64(i) + 0.5) - width/2.0
+			y := -(float64(j) + 0.5) + height/2.0
+			z := -float64(height) / (2.0 * math.Tan(fieldOfView/2.0))
+			direction := NewVec3(x, y, z).Normalize()
+			framebuffer[i+j*width] = CastRay(&origin, &direction, spheres)
 		}
 	}
 	fmt.Println("Framebuffer filled successfully")
@@ -33,16 +47,16 @@ func render() {
 
 	// Iterate over the framebuffer and fill the image based on the gradient corresponding to the vectors in the buffer
 	var max float64
-	for i := 0; i < height; i++ {
-		for j := 0; j < width; j++ {
-			v := framebuffer[j+i*width]
+	for j := 0; j < height; j++ {
+		for i := 0; i < width; i++ {
+			v := framebuffer[i+j*width]
 			max = math.Max(v.x, math.Max(v.y, v.z))
 			if max > 1 {
 				v = v.MultiplyScalar((1.0 / max))
 			}
 			r, g, b := v.ConvertToRGB()
 			c := color.RGBA{r, g, b, 255}
-			img.Set(j, i, c)
+			img.Set(i, j, c)
 		}
 	}
 
@@ -56,5 +70,15 @@ func render() {
 }
 
 func main() {
-	render()
+	render(createSpheres())
+}
+
+func createSpheres() []Sphere {
+	spheres := make([]Sphere, 0)
+	spheres = append(spheres, NewSphere(-3, 0, -16, 5, ivory))
+	spheres = append(spheres, NewSphere(-3.0, -1.5, -12, 2, redRubber))
+	spheres = append(spheres, NewSphere(3, -0.5, -18, 3, redRubber))
+	spheres = append(spheres, NewSphere(7, 5, -18, 4, ivory))
+	fmt.Println(spheres)
+	return spheres
 }
---
+++ objects.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// COLOR
+var bgColor = NewVec3(0.2, 0.7, 0.8)
+var objectColor = NewVec3(0.4, 0.4, 0.3)
+
+// Sphere : Define a sphere, with a center defined by a 3D vector and a scalar for the radius
+type Sphere struct {
+	center Vec3
+	radius float64
+	mat    Material
+}
+
+// Material : how do our spheres look?
+type Material struct {
+	diffuseColor Vec3
+}
+
+// NewMaterial : Return a new vec3 representing the diffuse_colour for a material
+func NewMaterial(x, y, z float64) Material {
+	return Material{
+		NewVec3(x, y, z),
+	}
+}
+
+// NewSphere : Return a new sphere object
+func NewSphere(x, y, z, r float64, mat Material) Sphere {
+	return Sphere{
+		NewVec3(x, y, z),
+		r,
+		mat,
+	}
+}
+
+// SceneIntersect : Cast a ray, determine what spheres are intersected, and what material is cast on
+// TODO : Figure SceneIntersect math out
+func SceneIntersect(origin, direction, hit, N *Vec3, mat *Material, spheres []Sphere) bool {
+	sphereDistance := math.MaxFloat64
+	for _, s := range spheres {
+		var distI float64
+		t0, intersect := s.RayIntersect(*origin, *direction, distI)
+		if intersect && t0 < sphereDistance {
+			sphereDistance = t0
+			k := origin.Add(direction.MultiplyScalar(t0))
+			hit.Copy(k)
+			n := k.Subtract(s.center).Normalize()
+			N.Copy(n)
+			mat.diffuseColor = s.mat.diffuseColor
+		}
+	}
+	return sphereDistance < 1000.0
+}
+
+// CastRay : Cast a ray toward all spheres in a scene, with a given origin of light and a direction, return the resulting 'reflection'(?)
+func CastRay(origin, direction *Vec3, spheres []Sphere) Vec3 {
+	point := &Vec3{}
+	N := &Vec3{}
+	mat := &Material{}
+
+	intersect := SceneIntersect(origin, direction, point, N, mat, spheres)
+	if !intersect {
+		return bgColor
+	}
+	return objectColor
+}
+
+// RayIntersect : http://www.lighthouse3d.com/tutorials/maths/ray-sphere-intersection/ (Check if a given ray intersects with a sphere)
+func (s Sphere) RayIntersect(origin, direction Vec3, t float64) (float64, bool) {
+
+	// Get the vector from the origin of the ray to the center of the sphere
+	L := s.center.Subtract(origin)
+
+	// Check how much the ray is in the direction of the sphere (https://math.stackexchange.com/questions/805954/what-does-the-dot-product-of-two-vectors-represent)
+	tc := L.DotProduct(direction)
+
+	// Sanity check, if its less than 0, its in the complete opposite direction (sphere is behind the ray origin)
+	if tc < 0.0 {
+		return t, false
+	}
+
+	// vec dot vec == sqrt(|vec|) (TODO: Double check understanding of this math)
+	// [https://physics.info/vector-multiplication/#:~:text=Since%20the%20projection%20of%20a,square%20of%20that%20vector's%20magnitude.&text=Applying%20this%20corollary%20to%20the,vector%20with%20itself%20is%20one.]
+	d2 := L.DotProduct(L) - (tc * tc)
+	r2 := s.radius * s.radius
+
+	// No intersection
+	if d2 > r2 {
+		return t, false
+	}
+
+	// Solve for magnitude
+	tlc := math.Sqrt(r2 - d2)
+
+	// Get interesection points
+	t = tc - tlc
+	t1 := tc + tlc
+
+	if t < 0.0 {
+		t = t1
+	}
+
+	if t < 0.0 {
+		return t, false
+	}
+
+	fmt.Println("interesection")
+	return t, true
+}
